9a1e3408a1222cfb02d7c80e024a7202f77011c2
Simon Longstaff <simonlngstaff@gmail.com>
2022-01-30 22:17:05 +0000

    Basic tag filter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,61 +1,66 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { HttpClientModule } from "@angular/common/http";
-import { AppComponent } from "./app.component";
-import { ProjectComponent } from "./components/project/project.component";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MatCardModule } from "@angular/material/card";
-import { CdkAccordionModule } from "@angular/cdk/accordion";
-import { TasksComponent } from "./components/tasks/tasks.component";
-import { ProjectListComponent } from "./components/project-list/project-list.component";
-import { TaskListComponent } from "./components/task-list/task-list.component";
-import { SubtaskTableComponent } from "./components/subtask-table/subtask-table.component";
-import { MatProgressBarModule } from "@angular/material/progress-bar";
-import { ProgressBarComponent } from "./components/Ui_Components/progress-bar/progress-bar.component";
-import { HeaderComponent } from "./components/root/header/header.component";
-import { AlertModalComponent } from "./components/root/alert-modal/alert-modal.component";
-import { NewProjectModalComponent } from "./components/new-project-modal/new-project-modal.component";
-import { MatIconModule } from "@angular/material/icon";
-import { NewSubtaskModalComponent } from "./components/new-subtask-modal/new-subtask-modal.component";
-import { WorkDoneModalComponent } from "./components/work-done-modal/work-done-modal.component";
-import { MatDatepickerModule } from "@angular/material/datepicker";
-import { MatNativeDateModule } from "@angular/material/core";
-import { MatTableModule } from "@angular/material/table";
-import { SubTaskRowComponent } from './components/sub-task-row/sub-task-row.component';
-import { AddTagModalComponent } from './components/add-tag-modal/add-tag-modal.component';
-import { TagDisplayComponent } from './components/tag-display/tag-display.component';
+import {NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
+import {HttpClientModule} from "@angular/common/http";
+import {AppComponent} from "./app.component";
+import {ProjectComponent} from "./components/project/project.component";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MatCardModule} from "@angular/material/card";
+import {CdkAccordionModule} from "@angular/cdk/accordion";
+import {TasksComponent} from "./components/tasks/tasks.component";
+import {ProjectListComponent} from "./components/project-list/project-list.component";
+import {TaskListComponent} from "./components/task-list/task-list.component";
+import {SubtaskTableComponent} from "./components/subtask-table/subtask-table.component";
+import {MatProgressBarModule} from "@angular/material/progress-bar";
+import {ProgressBarComponent} from "./components/Ui_Components/progress-bar/progress-bar.component";
+import {HeaderComponent} from "./components/root/header/header.component";
+import {AlertModalComponent} from "./components/root/alert-modal/alert-modal.component";
+import {NewProjectModalComponent} from "./components/new-project-modal/new-project-modal.component";
+import {MatIconModule} from "@angular/material/icon";
+import {NewSubtaskModalComponent} from "./components/new-subtask-modal/new-subtask-modal.component";
+import {WorkDoneModalComponent} from "./components/work-done-modal/work-done-modal.component";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatNativeDateModule} from "@angular/material/core";
+import {MatTableModule} from "@angular/material/table";
+import {SubTaskRowComponent} from './components/sub-task-row/sub-task-row.component';
+import {AddTagModalComponent} from './components/add-tag-modal/add-tag-modal.component';
+import {TagDisplayComponent} from './components/tag-display/tag-display.component';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from "@angular/material/input";
 
 @NgModule({
-	declarations: [
-		AppComponent,
-		ProjectComponent,
-		TasksComponent,
-		ProjectListComponent,
-		TaskListComponent,
-		SubtaskTableComponent,
-		ProgressBarComponent,
-		HeaderComponent,
-		AlertModalComponent,
-		NewProjectModalComponent,
-		NewSubtaskModalComponent,
-		WorkDoneModalComponent,
-  SubTaskRowComponent,
-  AddTagModalComponent,
-  TagDisplayComponent
-	],
-	imports: [
-		HttpClientModule,
-		BrowserModule,
-		BrowserAnimationsModule,
-		MatCardModule,
-		CdkAccordionModule,
-		MatProgressBarModule,
-		MatIconModule,
-		MatDatepickerModule,
-		MatNativeDateModule,
-		MatTableModule
-	],
-	providers: [],
-	bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    ProjectComponent,
+    TasksComponent,
+    ProjectListComponent,
+    TaskListComponent,
+    SubtaskTableComponent,
+    ProgressBarComponent,
+    HeaderComponent,
+    AlertModalComponent,
+    NewProjectModalComponent,
+    NewSubtaskModalComponent,
+    WorkDoneModalComponent,
+    SubTaskRowComponent,
+    AddTagModalComponent,
+    TagDisplayComponent
+  ],
+  imports: [
+    HttpClientModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    CdkAccordionModule,
+    MatProgressBarModule,
+    MatIconModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatTableModule,
+    MatSelectModule,
+    MatInputModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/app/components/project-list/project-list.component.ts
+++ src/app/components/project-list/project-list.component.ts
@@ -1,39 +1,52 @@
-import { Component, Input, OnInit } from "@angular/core";
-import { subtask } from "src/app/interfaces/subtask";
-import { Project } from "../../interfaces/Project";
-import { Task } from "../../interfaces/task";
-import { ProjectService } from "./../../services/project.service";
+import {Component, Input, OnInit} from "@angular/core";
+import {Project} from "../../interfaces/Project";
+import {ProjectService} from "../../services/project.service";
+import {Tag} from "../../interfaces/tag";
+import {TagsService} from "../../services/tags.service";
 
 @Component({
-	selector: "app-project-list",
-	templateUrl: "./project-list.component.html",
-	styleUrls: ["./project-list.component.css"]
+  selector: "app-project-list",
+  templateUrl: "./project-list.component.html",
+  styleUrls: ["./project-list.component.css"]
 })
 export class ProjectListComponent implements OnInit {
-	projects: Project[] = [];
+  projects: Project[] = [];
+  tags: Tag[] = [];
+  filterByTag: number = 0;
 
-	AddNewProject: boolean = false;
+  AddNewProject: boolean = false;
   AddNewTag: boolean = false;
+  selected: Tag | undefined;
 
-	constructor(private ProjectService: ProjectService) {}
+  constructor(private ProjectService: ProjectService, private TagsService: TagsService) {
+  }
 
-	ngOnInit(): void {
-		this.ProjectService.getProjects().subscribe(projects => {
-			this.projects = projects;
-		});
-	}
+  ngOnInit(): void {
+    this.ProjectService.getProjects().subscribe(projects => {
+      this.projects = projects;
+    });
 
-  handleDelete(deletedProject: Project){
+    this.TagsService.GetAllTags().subscribe(tags => {
+      this.tags = tags;
+    })
+  }
+
+  setTagFilter(): void {
+    //this.filterByTag = tagId;
+    console.log(this.selected)
+  }
+
+  handleDelete(deletedProject: Project) {
     this.projects.forEach((project, index) => {
-      if (project.id === deletedProject.id){
+      if (project.id === deletedProject.id) {
         this.projects.splice(index, 1);
       }
     })
   }
 
-	ShowAddNewModal(state: boolean): void {
-		this.AddNewProject = state;
-	}
+  ShowAddNewModal(state: boolean): void {
+    this.AddNewProject = state;
+  }
 
   RevealNewTagModal(): void {
     this.AddNewTag = !this.AddNewTag;
